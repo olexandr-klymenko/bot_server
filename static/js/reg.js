ece97c664934721dffa88fce0eb53f923c65de69
Oleksandr Klymenko <oklymenko@softserveinc.com>
2019-09-10 19:02:30 +0300

    Revert "Fix play"
    This reverts commit 47b5f0ff

--- static/js/reg.js
+++ static/js/reg.js
@@ -3,24 +3,14 @@ function spectator() {
 }
 
 function check_user_name() {
-    $.get("https://8080-dot-6949214-dot-devshell.appspot.com/labeling-sets/", function (data) {
-        console.log(data)
+    var name = $("#user").val();
+    $.get("/rest/check_user_name?" + name, function (data) {
+        if (data === 'False') {
+            $(location).attr("href", "/play?user=" + name);
+        } else {
+            alert(data);
+            alert("User name " + name + " is already registered");
+            $("#user").val("");
+        }
     });
-    // $.get("http://127.0.0.1:8000/labeling-sets", function (data) {
-    //     console.log(data)
-    // });
-    // let name = $("#user").val();
-    // $.post("/admin", {"command": "check_user_name",
-    //                   "args":  name}, function (data) {
-    //     if (data === 'False') {
-    //         $(location).attr("href", "/play?user=" + name);
-    //     } else {
-    //         alert(data);
-    //         alert("User name " + name + " is already registered");
-    //         $("#user").val("");
-    //     }
-    // });
 }
-
-// TODO: Move html elements creation to js
-// TODO: Add admin button link
